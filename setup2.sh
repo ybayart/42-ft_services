b0a495b017e2f3e572ddb996c90f88921ef697f4
hexanyn <hexanyn@gmail.com>
2019-11-26 07:35:17 +0100

    fini, manque plus que l'index nginx

--- setup2.sh
+++ setup2.sh
@@ -7,8 +7,7 @@
 #rm -rf ~/.minikube/ ~/goinfre/minikube
 #mkdir ~/goinfre/minikube
 #ln -s ~/goinfre/minikube ~/.minikube/ 
-#minikube start --vm-driver=virtualbox --cpus=4 --memory=8192m
-minikube start --vm-driver=virtualbox
+minikube start --vm-driver=virtualbox --cpus=4 --memory=8192m
 
 ###########################
 ## DOCKER                ##
@@ -17,6 +16,7 @@ minikube start --vm-driver=virtualbox
 eval $(minikube docker-env)
 
 docker build -t telegraf:v1 srcs/containers/telegraf/
+docker build -t ftps:v1 srcs/containers/ftps/
 docker build -t nginx:v1 srcs/containers/nginx/
 docker build -t wordpress:v1 srcs/containers/wordpress/
 docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
@@ -27,6 +27,10 @@ docker build -t influxdb:v1 srcs/containers/influxdb/
 ###########################
 ## DEPLOY                ##
 ###########################
+
+sudo kubectl create secret tls nginx --key srcs/containers/nginx/srcs/certs/server.key --cert srcs/containers/nginx/srcs/certs/server.crt
+
+sudo kubectl create -f srcs/ftps.yaml
 
 sudo kubectl create -f srcs/nginx.yaml
 
@@ -50,10 +54,11 @@ minikube addons enable ingress
 ## PORT-FORWARD         ##
 ##########################
 
-screen -dmS t1 sudo kubectl port-forward service/nginx 6022:22
-screen -dmS t2 sudo kubectl port-forward service/wordpress 5050
-screen -dmS t3 sudo kubectl port-forward service/phpmyadmin 5000
-screen -dmS t4 sudo kubectl port-forward service/grafana 3000
+screen -dmS t1 sudo kubectl port-forward service/ftps 6021:21
+screen -dmS t2 sudo kubectl port-forward service/nginx 6022:22
+screen -dmS t3 sudo kubectl port-forward service/wordpress 5050
+screen -dmS t4 sudo kubectl port-forward service/phpmyadmin 5000
+screen -dmS t5 sudo kubectl port-forward service/grafana 3000
 
 ###########################
 ## DASHBOARD             ##
---
+++ srcs/containers/ftps/srcs/start-ftps.sh
@@ -0,0 +1,6 @@
+sh /bin/startscript/start-telegraf.sh
+passwd ftpuser << PASSWORD
+pass
+pass
+PASSWORD
+screen -dmS ftps vsftpd
