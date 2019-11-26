961173cbece56b1b52b9896825338fc63798fe14
Yann BAYART <ybayart@e1r8p20.42.fr>
2019-11-26 18:36:09 +0100

    update

--- setup.sh
+++ setup.sh
@@ -2,12 +2,10 @@
 ## INIT                 ##
 ##########################
 
-brew update
-brew install kubernetes-cli minikube
 rm -rf ~/.minikube/ ~/goinfre/minikube
 mkdir ~/goinfre/minikube
 ln -s ~/goinfre/minikube ~/.minikube/ 
-minikube start --vm-driver=virtualbox
+minikube start --vm-driver=virtualbox --cpus=4 --memory=4096m
 
 ###########################
 ## DOCKER                ##
@@ -15,37 +13,31 @@ minikube start --vm-driver=virtualbox
 
 eval $(minikube docker-env)
 
+docker build -t telegraf:v1 srcs/containers/telegraf/
+docker build -t ftps:v1 srcs/containers/ftps/
 docker build -t nginx:v1 srcs/containers/nginx/
-docker build -t wordpress:v1 srcs/containers/wordpress
-docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin
-docker build -t grafana:v1 srcs/containers/grafana
-docker build -t mysql:v1 srcs/containers/mysql
+docker build -t wordpress:v1 srcs/containers/wordpress/
+docker build -t phpmyadmin:v1 srcs/containers/phpmyadmin/
+docker build -t grafana:v1 srcs/containers/grafana/
+docker build -t mysql:v1 srcs/containers/mysql/
+docker build -t influxdb:v1 srcs/containers/influxdb/
 
 ###########################
 ## DEPLOY                ##
 ###########################
 
-kubectl create -f srcs/nginx.yaml
+kubectl create secret tls nginx --key srcs/containers/nginx/srcs/certs/server.key --cert srcs/containers/nginx/srcs/certs/server.crt
 
-kubectl create -f srcs/mysql.yaml
+kubectl create -f srcs/ftps.yaml
+
+kubectl create -f srcs/nginx.yaml
 
 kubectl create -f srcs/wordpress.yaml
 kubectl create -f srcs/phpmyadmin.yaml
 kubectl create -f srcs/grafana.yaml
 
-###########################
-## SERVICE               ##
-###########################
-
-kubectl create -f srcs/nginx-svc.yaml
-
-kubectl create -f srcs/mysql-svc.yaml
-
-kubectl create -f srcs/wordpress-svc.yaml
-kubectl create -f srcs/phpmyadmin-svc.yaml
-kubectl create -f srcs/grafana-svc.yaml
-
-kubectl create -f srcs/mysql-link.yaml
+kubectl create -f srcs/mysql.yaml
+kubectl create -f srcs/influxdb.yaml
 
 ###########################
 ## INGRESS               ##
@@ -56,19 +48,18 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/mast
 
 minikube addons enable ingress
 
-kubectl create -f srcs/nginx-ingress.yaml
-
 ##########################
 ## PORT-FORWARD         ##
 ##########################
 
-screen -dmS t0 minikube dashboard
-screen -dmS t1 kubectl port-forward service/nginx-svc 6022:22
-screen -dmS t2 kubectl port-forward service/wordpress-svc 5050
-screen -dmS t3 kubectl port-forward service/phpmyadmin-svc 5000
-screen -dmS t4 kubectl port-forward service/grafana-svc 3000
+screen -dmS t1 kubectl port-forward service/ftps 6021:21
+screen -dmS t2 kubectl port-forward service/nginx 6022:22
+screen -dmS t3 kubectl port-forward service/wordpress 5050
+screen -dmS t4 kubectl port-forward service/phpmyadmin 5000
+screen -dmS t5 kubectl port-forward service/grafana 300
 
 ###########################
 ## DASHBOARD             ##
 ###########################
 
+screen -dmS t0 minikube dashboard
